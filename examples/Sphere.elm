45287ba3274b257a4078891d6edd35c8c5984939
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-06-11 02:06:23 -0400

    Add simple PBR example for testing

---
+++ examples/Sphere.elm
@@ -0,0 +1,95 @@
+module Sphere exposing (sphere)
+
+import Math.Vector3 exposing (Vec3)
+import OpenSolid.Geometry.Types exposing (..)
+import OpenSolid.Point3d as Point3d
+import OpenSolid.SceneGraph as SceneGraph
+import OpenSolid.SceneGraph.Lighting as Lighting
+import OpenSolid.SceneGraph.Material as Material
+
+
+sphere : Point3d -> Float -> SceneGraph.Geometry { vertexPosition : Vec3, vertexNormal : Vec3 }
+sphere centerPoint radius =
+    let
+        ( x0, y0, z0 ) =
+            Point3d.coordinates centerPoint
+
+        subdivisions =
+            36
+
+        angle =
+            2 * pi / subdivisions
+
+        angleValues =
+            List.range 0 subdivisions
+                |> List.map (\i -> toFloat i * angle)
+
+        pointsAndNormals =
+            angleValues
+                |> List.map
+                    (\theta ->
+                        angleValues
+                            |> List.map
+                                (\phi ->
+                                    ( Point3d
+                                        ( x0 + radius * sin phi * cos theta
+                                        , y0 + radius * sin phi * sin theta
+                                        , z0 + radius * cos phi
+                                        )
+                                    , Direction3d
+                                        ( sin phi * cos theta
+                                        , sin phi * sin theta
+                                        , cos phi
+                                        )
+                                    )
+                                )
+                    )
+                |> List.concat
+
+        startIndices =
+            List.range 0 (subdivisions - 1)
+
+        linearIndex thetaIndex phiIndex =
+            (thetaIndex % subdivisions) * subdivisions + (phiIndex % subdivisions)
+
+        faces =
+            startIndices
+                |> List.map
+                    (\leftThetaIndex ->
+                        startIndices
+                            |> List.map
+                                (\topPhiIndex ->
+                                    let
+                                        rightThetaIndex =
+                                            leftThetaIndex + 1
+
+                                        bottomPhiIndex =
+                                            topPhiIndex + 1
+
+                                        bottomLeftIndex =
+                                            linearIndex leftThetaIndex bottomPhiIndex
+
+                                        bottomRightIndex =
+                                            linearIndex rightThetaIndex bottomPhiIndex
+
+                                        topLeftIndex =
+                                            linearIndex leftThetaIndex topPhiIndex
+
+                                        topRightIndex =
+                                            linearIndex rightThetaIndex topPhiIndex
+                                    in
+                                    [ ( bottomLeftIndex
+                                      , bottomRightIndex
+                                      , topRightIndex
+                                      )
+                                    , ( bottomLeftIndex
+                                      , topRightIndex
+                                      , topLeftIndex
+                                      )
+                                    ]
+                                )
+                            |> List.concat
+                    )
+                |> List.concat
+    in
+    SceneGraph.indexedTrianglesWithNormals pointsAndNormals faces
---
+++ examples/Spheres.elm
@@ -0,0 +1,139 @@
+module Spheres exposing (..)
+
+import Color exposing (Color)
+import Html exposing (Html)
+import Html.Attributes as Attributes
+import OpenSolid.Direction3d as Direction3d
+import OpenSolid.Geometry.Types exposing (..)
+import OpenSolid.Point3d as Point3d
+import OpenSolid.SceneGraph as SceneGraph
+import OpenSolid.SceneGraph.Lighting as Lighting
+import OpenSolid.SceneGraph.Material as Material
+import OpenSolid.WebGL.Camera as Camera
+import OpenSolid.WebGL.Frame3d as Frame3d
+import Sphere
+import WebGL
+
+
+rgb : ( Float, Float, Float ) -> Color
+rgb ( red, green, blue ) =
+    Color.rgb (round (255 * red)) (round (255 * green)) (round (255 * blue))
+
+
+gold : SceneGraph.Material
+gold =
+    Material.physicallyBased
+        { baseColor = rgb ( 1, 0.86, 0.57 )
+        , roughness = 0.5
+        , metallic = 1
+        }
+
+
+aluminum : SceneGraph.Material
+aluminum =
+    Material.physicallyBased
+        { baseColor = rgb ( 0.96, 0.96, 0.97 )
+        , roughness = 0.5
+        , metallic = 1
+        }
+
+
+blackPlastic : SceneGraph.Material
+blackPlastic =
+    Material.physicallyBased
+        { baseColor = Color.black
+        , roughness = 0.3
+        , metallic = 0
+        }
+
+
+whitePlastic : SceneGraph.Material
+whitePlastic =
+    Material.physicallyBased
+        { baseColor = Color.white
+        , roughness = 0.75
+        , metallic = 0
+        }
+
+
+direction phi theta =
+    Direction3d
+        ( -(sin phi * cos theta)
+        , -(sin phi * sin theta)
+        , -(cos phi)
+        )
+
+
+lightDirection =
+    direction (degrees 30) (degrees 90)
+
+
+lightColor =
+    rgb ( 0.5, 0.5, 0.5 )
+
+
+lighting =
+    Lighting.single (Lighting.directional lightColor lightDirection)
+
+
+goldSphere =
+    Sphere.sphere (Point3d ( 2, 2, 0 )) 1
+        |> SceneGraph.shaded gold lighting
+
+
+aluminumSphere =
+    Sphere.sphere (Point3d ( 2, -2, 0 )) 1
+        |> SceneGraph.shaded aluminum lighting
+
+
+blackPlasticSphere =
+    Sphere.sphere (Point3d ( -2, -2, 0 )) 1
+        |> SceneGraph.shaded blackPlastic lighting
+
+
+whitePlasticSphere =
+    Sphere.sphere (Point3d ( -2, 2, 0 )) 1
+        |> SceneGraph.shaded whitePlastic lighting
+
+
+scene =
+    SceneGraph.group
+        [ goldSphere
+        , aluminumSphere
+        , blackPlasticSphere
+        , whitePlasticSphere
+        ]
+
+
+cameraFrame =
+    Frame3d.lookAt
+        { focalPoint = Point3d.origin
+        , eyePoint = Point3d ( 10, 10, 10 )
+        , upDirection = Direction3d.positiveZ
+        }
+
+
+width =
+    1024
+
+
+height =
+    768
+
+
+camera =
+    Camera.perspective
+        { frame = cameraFrame
+        , screenWidth = width
+        , screenHeight = height
+        , verticalFov = degrees 30
+        , zNear = 0.1
+        , zFar = 100
+        }
+
+
+main : Html Never
+main =
+    WebGL.toHtml
+        [ Attributes.width width, Attributes.height height ]
+        (SceneGraph.toEntities camera scene)
