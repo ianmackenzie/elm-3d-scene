a6664f627ad42239cae49b2216ac6fc5eb3e6009
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2017-09-07 13:44:01 -0400

    Add bearings and animation

--- examples/Sprocket.elm
+++ examples/Sprocket.elm
@@ -1,5 +1,6 @@
 module Sprocket exposing (..)
 
+import AnimationFrame
 import Html exposing (Attribute, Html)
 import Html.Attributes as Attributes
 import Html.Events as Events
@@ -8,17 +9,20 @@ import Materials
 import Math.Vector3 exposing (Vec3, vec3)
 import Mouse
 import OpenSolid.Arc2d as Arc2d
+import OpenSolid.Arc3d as Arc3d
 import OpenSolid.Axis2d as Axis2d
 import OpenSolid.Axis3d as Axis3d exposing (Axis3d)
 import OpenSolid.Body3d as Body3d exposing (Body3d)
 import OpenSolid.BoundaryType as BoundaryType
 import OpenSolid.Circle2d as Circle2d
 import OpenSolid.Curve2d as Curve2d exposing (Curve2d)
+import OpenSolid.Curve3d as Curve3d
 import OpenSolid.Direction2d as Direction2d exposing (Direction2d)
 import OpenSolid.Direction3d as Direction3d exposing (Direction3d)
 import OpenSolid.Frame2d as Frame2d exposing (Frame2d)
 import OpenSolid.Frame3d as Frame3d exposing (Frame3d)
 import OpenSolid.LineSegment2d as LineSegment2d
+import OpenSolid.LineSegment3d as LineSegment3d
 import OpenSolid.Point2d as Point2d exposing (Point2d)
 import OpenSolid.Point3d as Point3d exposing (Point3d)
 import OpenSolid.Rectangle2d as Rectangle2d exposing (Rectangle2d)
@@ -28,11 +32,14 @@ import OpenSolid.Scene.Geometry as Geometry exposing (Geometry)
 import OpenSolid.Scene.Light as Light exposing (Light)
 import OpenSolid.Scene.Node as Node exposing (Node)
 import OpenSolid.SketchPlane3d as SketchPlane3d exposing (SketchPlane3d)
+import OpenSolid.Surface3d as Surface3d
 import OpenSolid.Vector2d as Vector2d exposing (Vector2d)
 import OpenSolid.Vector3d as Vector3d exposing (Vector3d)
 import OpenSolid.WebGL.Camera as Camera exposing (Camera)
+import Shapes
 import SingleTouch
 import Task
+import Time exposing (Time)
 import Touch exposing (Touch, TouchEvent(..))
 import Window
 
@@ -45,6 +52,7 @@ type Msg
     | PointerMovedTo Point2d
     | StopRotating
     | NumTeethChanged Int
+    | Tick Time
 
 
 type alias Model =
@@ -52,6 +60,7 @@ type alias Model =
     , placementFrame : Frame3d
     , numTeeth : Int
     , sprocketNode : () -> Node
+    , rotationAngle : Float
     }
 
 
@@ -74,7 +83,7 @@ camera =
     Camera.perspective
         { frame =
             Camera.lookAt
-                { eyePoint = Point3d.withCoordinates ( 8, 0, 4 )
+                { eyePoint = Point3d.withCoordinates ( 6, 0, 3 )
                 , focalPoint = Point3d.origin
                 , upDirection = Direction3d.z
                 }
@@ -103,7 +112,9 @@ createSprocketNode numTeeth =
                 , rollerDiameter = 0.125
                 }
     in
-    Geometry.body tolerance sprocketBody |> Geometry.shaded Materials.aluminum
+    Geometry.body tolerance sprocketBody
+        |> Geometry.shaded Materials.aluminum
+        |> Node.translateBy (Vector3d.withComponents ( 0, 0, -0.25 ))
 
 
 mousePositionToPoint : Mouse.Position -> Point2d
@@ -127,6 +138,7 @@ init =
             , placementFrame = Frame3d.xyz
             , numTeeth = initialNumTeeth
             , sprocketNode = always (createSprocketNode initialNumTeeth)
+            , rotationAngle = 0
             }
     in
     ( model, Cmd.none )
@@ -163,6 +175,152 @@ roundedRectangle =
 roundedBlock : Body3d
 roundedBlock =
     Body3d.extrusion roundedRectangle SketchPlane3d.xy 2
+
+
+bearingRaces : { bore : Float, outerDiameter : Float, externalRaceDiameter : Float, internalRaceDiameter : Float, width : Float, ballDiameter : Float } -> Body3d
+bearingRaces { bore, outerDiameter, externalRaceDiameter, internalRaceDiameter, width, ballDiameter } =
+    let
+        halfWidth =
+            width / 2
+
+        innerRadius =
+            bore / 2
+
+        outerRadius =
+            outerDiameter / 2
+
+        innerRaceOuterRadius =
+            internalRaceDiameter / 2
+
+        outerRaceInnerRadius =
+            externalRaceDiameter / 2
+
+        ballRadius =
+            ballDiameter / 2
+
+        gap =
+            outerRaceInnerRadius - innerRaceOuterRadius
+
+        trackHalfWidth =
+            sqrt (ballRadius * ballRadius - gap * gap / 4)
+
+        trackSweptAngle =
+            2 * acos (gap / (2 * ballRadius))
+
+        p1 =
+            Point2d.withCoordinates ( -halfWidth, innerRadius )
+
+        p2 =
+            Point2d.withCoordinates ( halfWidth, innerRadius )
+
+        p3 =
+            Point2d.withCoordinates ( halfWidth, innerRaceOuterRadius )
+
+        p4 =
+            Point2d.withCoordinates ( trackHalfWidth, innerRaceOuterRadius )
+
+        p5 =
+            Point2d.withCoordinates ( -trackHalfWidth, innerRaceOuterRadius )
+
+        p6 =
+            Point2d.withCoordinates ( -halfWidth, innerRaceOuterRadius )
+
+        p7 =
+            Point2d.withCoordinates ( -halfWidth, outerRaceInnerRadius )
+
+        p8 =
+            Point2d.withCoordinates ( -trackHalfWidth, outerRaceInnerRadius )
+
+        p9 =
+            Point2d.withCoordinates ( trackHalfWidth, outerRaceInnerRadius )
+
+        p10 =
+            Point2d.withCoordinates ( halfWidth, outerRaceInnerRadius )
+
+        p11 =
+            Point2d.withCoordinates ( halfWidth, outerRadius )
+
+        p12 =
+            Point2d.withCoordinates ( -halfWidth, outerRadius )
+
+        centerPoint =
+            Point2d.withCoordinates
+                ( 0
+                , (innerRaceOuterRadius + outerRaceInnerRadius) / 2
+                )
+
+        lineSegment firstPoint secondPoint =
+            LineSegment2d.from firstPoint secondPoint
+                |> LineSegment3d.on SketchPlane3d.zx
+                |> Curve3d.lineSegment
+
+        surface edge =
+            Surface3d.revolution edge Axis3d.z (2 * pi)
+
+        outerSurface =
+            surface (lineSegment p11 p12)
+
+        innerSurface =
+            surface (lineSegment p1 p2)
+
+        outerRaceFrontSurface =
+            surface (lineSegment p10 p11)
+
+        outerRaceBackSurface =
+            surface (lineSegment p12 p7)
+
+        outerRaceInnerFrontSurface =
+            surface (lineSegment p9 p10)
+
+        outerRaceInnerBackSurface =
+            surface (lineSegment p7 p8)
+
+        innerRaceFrontSurface =
+            surface (lineSegment p2 p3)
+
+        innerRaceBackSurface =
+            surface (lineSegment p6 p1)
+
+        innerRaceOuterFrontSurface =
+            surface (lineSegment p3 p4)
+
+        innerRaceOuterBackSurface =
+            surface (lineSegment p5 p6)
+
+        outerTrackArc =
+            Arc2d.with
+                { centerPoint = centerPoint
+                , startPoint = p8
+                , sweptAngle = -trackSweptAngle
+                }
+
+        innerTrackArc =
+            Arc2d.with
+                { centerPoint = centerPoint
+                , startPoint = p4
+                , sweptAngle = -trackSweptAngle
+                }
+
+        outerTrackSurface =
+            surface (Curve3d.arc (Arc3d.on SketchPlane3d.zx outerTrackArc))
+
+        innerTrackSurface =
+            surface (Curve3d.arc (Arc3d.on SketchPlane3d.zx innerTrackArc))
+    in
+    Body3d.fromSurfaces
+        [ outerSurface
+        , innerSurface
+        , outerRaceFrontSurface
+        , outerRaceBackSurface
+        , innerRaceFrontSurface
+        , innerRaceBackSurface
+        , outerRaceInnerFrontSurface
+        , outerRaceInnerBackSurface
+        , innerRaceOuterFrontSurface
+        , innerRaceOuterBackSurface
+        , outerTrackSurface
+        , innerTrackSurface
+        ]
 
 
 sprocketWith : { bore : Float, pitch : Float, numTeeth : Int, thickness : Float, rollerDiameter : Float, toothHeight : Float } -> Body3d
@@ -293,6 +451,7 @@ hub =
                 (turns 1)
     in
     Body3d.extrusion region SketchPlane3d.xy 0.5
+        |> Body3d.translateBy (Vector3d.withComponents ( 0, 0, -0.25 ))
 
 
 shaft : Body3d
@@ -319,11 +478,73 @@ tolerance =
     0.001
 
 
-staticNodes : List Node
+staticNodes : Node
 staticNodes =
-    [ Geometry.body tolerance hub |> Geometry.shaded Materials.aluminum
-    , Geometry.body tolerance shaft |> Geometry.shaded Materials.gold
-    ]
+    Node.group
+        [ Geometry.body tolerance hub |> Geometry.shaded Materials.aluminum
+        , Geometry.body tolerance shaft |> Geometry.shaded Materials.gold
+        ]
+
+
+bearingNode : Node
+bearingNode =
+    let
+        externalRaceDiameter =
+            1.0625
+
+        internalRaceDiameter =
+            0.6875
+
+        ballDiameter =
+            0.25
+
+        races =
+            bearingRaces
+                { bore = 0.5
+                , outerDiameter = 1.25
+                , externalRaceDiameter = externalRaceDiameter
+                , internalRaceDiameter = internalRaceDiameter
+                , width = 0.375
+                , ballDiameter = ballDiameter
+                }
+
+        racesNode =
+            Geometry.body tolerance races
+                |> Geometry.shaded Materials.chromium
+
+        numBalls =
+            8
+
+        ballAngle =
+            2 * pi / toFloat numBalls
+
+        ballPathRadius =
+            (internalRaceDiameter + externalRaceDiameter) / 4
+
+        firstBallPoint =
+            Point3d.withCoordinates ( ballPathRadius, 0, 0 )
+
+        firstBall =
+            Shapes.sphere firstBallPoint (ballDiameter / 2)
+                |> Geometry.shaded Materials.whitePlastic
+
+        toBall index =
+            let
+                angle =
+                    toFloat index * ballAngle
+            in
+            Node.rotateAround Axis3d.z angle firstBall
+
+        ballNodes =
+            List.map toBall (List.range 0 (numBalls - 1))
+
+        singleBearing =
+            Node.group (racesNode :: ballNodes)
+    in
+    Node.group
+        [ singleBearing |> Node.translateBy (Vector3d.withComponents ( 0, 0, 1.125 ))
+        , singleBearing |> Node.translateBy (Vector3d.withComponents ( 0, 0, -1.125 ))
+        ]
 
 
 lights : List Light
@@ -343,7 +564,7 @@ lights =
 
 
 view : Model -> Html Msg
-view { placementFrame, numTeeth, sprocketNode } =
+view { placementFrame, numTeeth, sprocketNode, rotationAngle } =
     let
         sliderAttributes =
             [ Attributes.style [ ( "width", toString screenWidth ++ "px" ) ] ]
@@ -354,8 +575,14 @@ view { placementFrame, numTeeth, sprocketNode } =
             , step = 1
             }
 
+        shaftNode =
+            Node.group [ sprocketNode (), staticNodes ]
+
         scene =
-            Node.group (sprocketNode () :: staticNodes)
+            Node.group
+                [ shaftNode |> Node.rotateAround Axis3d.z rotationAngle
+                , bearingNode |> Node.rotateAround Axis3d.z (rotationAngle / 2)
+                ]
     in
     Html.div []
         [ Html.div dragAttributes
@@ -432,6 +659,13 @@ update message model =
             , Cmd.none
             )
 
+        Tick time ->
+            ( { model
+                | rotationAngle = model.rotationAngle + Time.inSeconds time * degrees 45
+              }
+            , Cmd.none
+            )
+
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
@@ -440,10 +674,11 @@ subscriptions model =
             Sub.batch
                 [ Mouse.moves (mousePositionToPoint >> PointerMovedTo)
                 , Mouse.ups (always StopRotating)
+                , AnimationFrame.diffs Tick
                 ]
 
         Nothing ->
-            Sub.none
+            AnimationFrame.diffs Tick
 
 
 main : Program Never Model Msg
