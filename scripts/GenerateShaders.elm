125851de0f8215a9ca6f382b073ec6f983bc0544
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2019-12-15 23:40:47 -0500

    Start fleshing out GLSL generation

--- scripts/GenerateShaders.elm
+++ scripts/GenerateShaders.elm
@@ -5,9 +5,40 @@ import Glsl
 import Script exposing (Script)
 
 
+position : Glsl.Attribute
+position =
+    Glsl.attribute "position" Glsl.vec3
+
+
+modelScale : Glsl.Uniform
+modelScale =
+    Glsl.uniform "modelScale" Glsl.float
+
+
+modelMatrix : Glsl.Uniform
+modelMatrix =
+    Glsl.uniform "modelMatrix" Glsl.mat4
+
+
+viewMatrix : Glsl.Uniform
+viewMatrix =
+    Glsl.uniform "viewMatrix" Glsl.mat4
+
+
+sceneProperties : Glsl.Uniform
+sceneProperties =
+    Glsl.uniform "sceneProperties" Glsl.mat4
+
+
+interpolatedPosition : Glsl.Varying
+interpolatedPosition =
+    Glsl.varying "interpolatedPosition" Glsl.vec3
+
+
 project : Glsl.Function
 project =
-    Glsl.function """
+    Glsl.function
+        """
         vec4 project(vec4 position, vec4 projectionProperties) {
             float n = projectionProperties[0];
             float a = projectionProperties[1];
@@ -19,8 +50,28 @@ project =
                 (-position.z - 2.0 * n),
                 -position.z
             );
-        }"""
+        }
+        """
         []
+
+
+plainVertex : String
+plainVertex =
+    Glsl.program
+        { attributes = [ position ]
+        , uniforms = [ modelScale, modelMatrix, viewMatrix, sceneProperties ]
+        , varyings = [ interpolatedPosition ]
+        }
+        """
+        void main () {
+            vec4 scaledPosition = vec4(modelScale * position, 1.0);
+            vec4 transformedPosition = modelMatrix * scaledPosition;
+            vec4 projectionProperties = sceneProperties[0];
+            gl_Position = project(viewMatrix * transformedPosition, projectionProperties);
+            interpolatedPosition = transformedPosition.xyz;
+        }
+        """
+        [ project ]
 
 
 script : Script.Init -> Script Int ()
--- scripts/Glsl.elm
+++ scripts/Glsl.elm
@@ -1,8 +1,20 @@
 module Glsl exposing
-    ( Function
+    ( Attribute
+    , Function
     , Type(..)
+    , Uniform
+    , Varying
+    , attribute
+    , float
     , function
+    , mat3
+    , mat4
     , program
+    , uniform
+    , varying
+    , vec2
+    , vec3
+    , vec4
     )
 
 
@@ -14,18 +26,72 @@ type Function
 
 
 type Type
-    = Float
-    | Vec2
-    | Vec3
-    | Vec4
-    | Mat3
-    | Mat4
-    | Texture
+    = Type String
+
+
+float : Type
+float =
+    Type "float"
+
+
+vec2 : Type
+vec2 =
+    Type "vec2"
+
+
+vec3 : Type
+vec3 =
+    Type "vec3"
+
+
+vec4 : Type
+vec4 =
+    Type "vec4"
+
+
+mat3 : Type
+mat3 =
+    Type "mat3"
+
+
+mat4 : Type
+mat4 =
+    Type "mat4"
+
+
+type Attribute
+    = Attribute String Type
+
+
+type Uniform
+    = Uniform String Type
+
+
+type Varying
+    = Varying String Type
+
+
+attribute : String -> Type -> Attribute
+attribute =
+    Attribute
+
+
+uniform : String -> Type -> Uniform
+uniform =
+    Uniform
+
+
+varying : String -> Type -> Varying
+varying =
+    Varying
 
 
 function : String -> List Function -> Function
 function source dependencies =
-    Function { source = source, dependencies = dependencies }
+    Function
+        { source = stripExtraWhitespace source
+        , dependencies = dependencies
+        }
 
 
 functionSource : Function -> String
@@ -34,16 +100,71 @@ functionSource (Function { source }) =
 
 
 type alias Types =
-    { attributes : List ( String, Type )
-    , uniforms : List ( String, Type )
-    , varying : List ( String, Type )
+    { attributes : List Attribute
+    , uniforms : List Uniform
+    , varyings : List Varying
     }
 
 
 program : Types -> String -> List Function -> String
 program types source dependencies =
+    String.join "\n\n" <|
+        List.filter (not << String.isEmpty) <|
+            "precision mediump float;"
+                :: attributesBlock types.attributes
+                :: uniformsBlock types.uniforms
+                :: varyingsBlock types.varyings
+                :: stripExtraWhitespace source
+                :: List.map functionSource dependencies
+
+
+attributesBlock : List Attribute -> String
+attributesBlock attributes =
+    String.join "\n" (List.map attributeLine attributes)
+
+
+attributeLine : Attribute -> String
+attributeLine (Attribute name (Type typeName)) =
+    "attribute " ++ typeName ++ " " ++ name ++ ";"
+
+
+uniformsBlock : List Uniform -> String
+uniformsBlock uniforms =
+    String.join "\n" (List.map uniformLine uniforms)
+
+
+uniformLine : Uniform -> String
+uniformLine (Uniform name (Type typeName)) =
+    "uniform " ++ typeName ++ " " ++ name ++ ";"
+
+
+varyingsBlock : List Varying -> String
+varyingsBlock varyings =
+    String.join "\n" (List.map varyingLine varyings)
+
+
+varyingLine : Varying -> String
+varyingLine (Varying name (Type typeName)) =
+    "varying " ++ typeName ++ " " ++ name ++ ";"
+
+
+indentation : String -> Maybe Int
+indentation string =
+    if String.isEmpty (String.trim string) then
+        Nothing
+
+    else
+        Just (String.length string - String.length (String.trimLeft string))
+
+
+stripExtraWhitespace : String -> String
+stripExtraWhitespace code =
     let
-        shaderSource =
-            String.join "\n" (source :: List.map functionSource dependencies)
+        lines =
+            String.lines code
+
+        minIndentation =
+            List.minimum (List.filterMap indentation lines)
+                |> Maybe.withDefault 0
     in
-    ""
+    String.trim (String.join "\n" (List.map (String.dropLeft minIndentation) lines))
