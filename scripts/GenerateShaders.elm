60eb114acb9c6b7c1c5381fd4446e5ce20e40486
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2019-12-16 14:29:44 -0500

    Continue work on shader generation
    - Add support for shared constants
    - Tweak definition of attributes, uniforms, varyings and constants to match how they would be declared in GLSL (type then name)
    - Add a couple more shaders (point vertex shader, smooth vertex shader, shadow vertex shader)
    - Write shaders to files to check that they're being generated correctly

--- scripts/GenerateShaders.elm
+++ scripts/GenerateShaders.elm
@@ -1,43 +1,80 @@
 module GenerateShaders exposing (main)
 
-import Common
+import Common exposing (handleError)
 import Glsl
 import Script exposing (Script)
+import Script.Directory as Directory
+import Script.File as File
 
 
 position : Glsl.Attribute
 position =
-    Glsl.attribute "position" Glsl.vec3
+    Glsl.attribute Glsl.vec3 "position"
+
+
+normal : Glsl.Attribute
+normal =
+    Glsl.attribute Glsl.vec3 "normal"
 
 
 modelScale : Glsl.Uniform
 modelScale =
-    Glsl.uniform "modelScale" Glsl.float
+    Glsl.uniform Glsl.float "modelScale"
 
 
 modelMatrix : Glsl.Uniform
 modelMatrix =
-    Glsl.uniform "modelMatrix" Glsl.mat4
+    Glsl.uniform Glsl.mat4 "modelMatrix"
 
 
 viewMatrix : Glsl.Uniform
 viewMatrix =
-    Glsl.uniform "viewMatrix" Glsl.mat4
+    Glsl.uniform Glsl.mat4 "viewMatrix"
 
 
 sceneProperties : Glsl.Uniform
 sceneProperties =
-    Glsl.uniform "sceneProperties" Glsl.mat4
+    Glsl.uniform Glsl.mat4 "sceneProperties"
+
+
+lightSource : Glsl.Uniform
+lightSource =
+    Glsl.uniform Glsl.mat4 "lightSource"
+
+
+pointRadius : Glsl.Uniform
+pointRadius =
+    Glsl.uniform Glsl.float "pointRadius"
 
 
 interpolatedPosition : Glsl.Varying
 interpolatedPosition =
-    Glsl.varying "interpolatedPosition" Glsl.vec3
+    Glsl.varying Glsl.vec3 "interpolatedPosition"
+
+
+interpolatedNormal : Glsl.Varying
+interpolatedNormal =
+    Glsl.varying Glsl.vec3 "interpolatedNormal"
+
+
+disabledLightSource : Glsl.Constant
+disabledLightSource =
+    Glsl.constant Glsl.float "DISABLED_LIGHT_SOURCE" "0.0"
+
+
+directionalLightSource : Glsl.Constant
+directionalLightSource =
+    Glsl.constant Glsl.float "DIRECTIONAL_LIGHT_SOURCE" "1.0"
+
+
+pointLightSource : Glsl.Constant
+pointLightSource =
+    Glsl.constant Glsl.float "POINT_LIGHT_SOURCE" "2.0"
 
 
 project : Glsl.Function
 project =
-    Glsl.function
+    Glsl.function { dependencies = [], constants = [] }
         """
         vec4 project(vec4 position, vec4 projectionProperties) {
             float n = projectionProperties[0];
@@ -52,31 +89,125 @@ project =
             );
         }
         """
-        []
 
 
-plainVertex : String
-plainVertex =
+plainVertexShader : String
+plainVertexShader =
     Glsl.program
         { attributes = [ position ]
         , uniforms = [ modelScale, modelMatrix, viewMatrix, sceneProperties ]
         , varyings = [ interpolatedPosition ]
+        , constants = []
+        , functions = [ project ]
         }
         """
         void main () {
             vec4 scaledPosition = vec4(modelScale * position, 1.0);
             vec4 transformedPosition = modelMatrix * scaledPosition;
-            vec4 projectionProperties = sceneProperties[0];
-            gl_Position = project(viewMatrix * transformedPosition, projectionProperties);
+            gl_Position = project(viewMatrix * transformedPosition, sceneProperties[0]);
             interpolatedPosition = transformedPosition.xyz;
         }
         """
-        [ project ]
+
+
+pointVertexShader : String
+pointVertexShader =
+    Glsl.program
+        { attributes = [ position ]
+        , uniforms = [ modelScale, modelMatrix, pointRadius, viewMatrix, sceneProperties ]
+        , varyings = []
+        , constants = []
+        , functions = [ project ]
+        }
+        """
+        void main () {
+            vec4 scaledPosition = vec4(modelScale * position, 1.0);
+            vec4 transformedPosition = modelMatrix * scaledPosition;
+            gl_Position = project(viewMatrix * transformedPosition, sceneProperties[0]);
+            gl_PointSize = 2.0 * pointRadius + 2.0;
+        }
+        """
+
+
+smoothVertexShader : String
+smoothVertexShader =
+    Glsl.program
+        { attributes = [ position, normal ]
+        , uniforms = [ modelScale, modelMatrix, viewMatrix, sceneProperties ]
+        , varyings = [ interpolatedPosition, interpolatedNormal ]
+        , constants = []
+        , functions = [ project ]
+        }
+        """
+        void main () {
+            vec4 scaledPosition = vec4(modelScale * position, 1.0);
+            vec4 transformedPosition = modelMatrix * scaledPosition;
+            gl_Position = project(viewMatrix * transformedPosition, sceneProperties[0]);
+            interpolatedPosition = transformedPosition.xyz;
+            interpolatedNormal = (modelMatrix * vec4(normal, 0.0)).xyz;
+        }
+        """
+
+
+getDirectionToLight : Glsl.Function
+getDirectionToLight =
+    Glsl.function
+        { dependencies = []
+        , constants = [ directionalLightSource, pointLightSource ]
+        }
+        """
+        vec3 getDirectionToLight(vec3 surfacePosition, vec4 xyz_type, vec4 rgb_radius) {
+            float lightSourceType = xyz_type.w;
+            if (lightSourceType == DIRECTIONAL_LIGHT_SOURCE) {
+                return xyz_type.xyz;
+            } else if (lightSourceType == POINT_LIGHT_SOURCE) {
+                vec3 lightPosition = xyz_type.xyz;
+                return normalize(lightPosition - surfacePosition);
+            }
+        }
+        """
+
+
+shadowVertexShader : String
+shadowVertexShader =
+    Glsl.program
+        { attributes = [ position, normal ]
+        , uniforms = [ modelScale, modelMatrix, viewMatrix, sceneProperties, lightSource ]
+        , varyings = []
+        , constants = []
+        , functions = [ project, getDirectionToLight ]
+        }
+        """
+        void main () {
+            vec4 scaledPosition = vec4(modelScale * position, 1.0);
+            vec4 transformedPosition = modelMatrix * scaledPosition;
+            vec3 transformedNormal = (modelMatrix * vec4(normal, 0.0)).xyz;
+            vec4 xyz_type = lightSource[0];
+            vec4 rgb_radius = lightSource[1];
+            vec3 directionToLight = getDirectionToLight(position, xyz_type, rgb_radius);
+            vec3 offset = vec3(0.0, 0.0, 0.0);
+            if (dot(directionToLight, transformedNormal) <= 0.0) {
+                offset = -1.0e9 * directionToLight;
+            }
+            vec4 offsetPosition = transformedPosition + vec4(offset, 0.0);
+            gl_Position = project(viewMatrix * offsetPosition, sceneProperties[0]);
+        }
+        """
 
 
 script : Script.Init -> Script Int ()
-script init =
-    Script.fail 1
+script { workingDirectory } =
+    let
+        write shader fileName =
+            File.write shader (workingDirectory |> Directory.file fileName)
+    in
+    Script.do
+        [ write plainVertexShader "plainVertexShader.glsl"
+        , write pointVertexShader "pointVertexShader.glsl"
+        , write smoothVertexShader "smoothVertexShader.glsl"
+        , write shadowVertexShader "shadowVertexShader.glsl"
+        ]
+        |> Script.onError (handleError .message)
 
 
 main : Script.Program
--- scripts/Glsl.elm
+++ scripts/Glsl.elm
@@ -1,10 +1,12 @@
 module Glsl exposing
     ( Attribute
+    , Constant
     , Function
     , Type(..)
     , Uniform
     , Varying
     , attribute
+    , constant
     , float
     , function
     , mat3
@@ -19,10 +21,7 @@ module Glsl exposing
 
 
 type Function
-    = Function
-        { source : String
-        , dependencies : List Function
-        }
+    = Function String (List Function) (List Constant)
 
 
 type Type
@@ -60,62 +59,125 @@ mat4 =
 
 
 type Attribute
-    = Attribute String Type
+    = Attribute Type String
 
 
 type Uniform
-    = Uniform String Type
+    = Uniform Type String
 
 
 type Varying
-    = Varying String Type
+    = Varying Type String
 
 
-attribute : String -> Type -> Attribute
+type Constant
+    = Constant Type String String
+
+
+attribute : Type -> String -> Attribute
 attribute =
     Attribute
 
 
-uniform : String -> Type -> Uniform
+uniform : Type -> String -> Uniform
 uniform =
     Uniform
 
 
-varying : String -> Type -> Varying
+varying : Type -> String -> Varying
 varying =
     Varying
 
 
-function : String -> List Function -> Function
-function source dependencies =
-    Function
-        { source = stripExtraWhitespace source
-        , dependencies = dependencies
-        }
+constant : Type -> String -> String -> Constant
+constant =
+    Constant
+
+
+function : { dependencies : List Function, constants : List Constant } -> String -> Function
+function { dependencies, constants } source =
+    Function (stripExtraWhitespace source) dependencies constants
 
 
 functionSource : Function -> String
-functionSource (Function { source }) =
+functionSource (Function source dependencies constants) =
     source
 
 
-type alias Types =
+addFunctionSource : String -> List String -> List String
+addFunctionSource source accumulated =
+    if List.member source accumulated then
+        accumulated
+
+    else
+        accumulated ++ [ source ]
+
+
+collectFunctionSources : List Function -> List String -> List String
+collectFunctionSources functions accumulated =
+    case functions of
+        (Function source dependencies constants) :: rest ->
+            accumulated
+                |> collectFunctionSources dependencies
+                |> addFunctionSource source
+                |> collectFunctionSources rest
+
+        [] ->
+            accumulated
+
+
+addConstant : Constant -> List String -> List String
+addConstant (Constant (Type typeName) name value) accumulated =
+    let
+        constantLine =
+            "const " ++ typeName ++ " " ++ name ++ " = " ++ value ++ ";"
+    in
+    if List.member constantLine accumulated then
+        accumulated
+
+    else
+        accumulated ++ [ constantLine ]
+
+
+collectFunctionConstants : List Function -> List String -> List String
+collectFunctionConstants functions accumulated =
+    case functions of
+        (Function source dependencies constants) :: rest ->
+            List.foldl addConstant (collectFunctionConstants dependencies accumulated) constants
+                |> collectFunctionConstants rest
+
+        [] ->
+            accumulated
+
+
+program :
     { attributes : List Attribute
     , uniforms : List Uniform
     , varyings : List Varying
+    , constants : List Constant
+    , functions : List Function
     }
+    -> String
+    -> String
+program { attributes, uniforms, varyings, constants, functions } source =
+    let
+        allFunctions =
+            String.join "\n\n" (collectFunctionSources functions [])
 
-
-program : Types -> String -> List Function -> String
-program types source dependencies =
+        allConstants =
+            String.join "\n" <|
+                List.foldl addConstant (collectFunctionConstants functions []) constants
+    in
     String.join "\n\n" <|
         List.filter (not << String.isEmpty) <|
-            "precision mediump float;"
-                :: attributesBlock types.attributes
-                :: uniformsBlock types.uniforms
-                :: varyingsBlock types.varyings
-                :: stripExtraWhitespace source
-                :: List.map functionSource dependencies
+            [ "precision mediump float;"
+            , attributesBlock attributes
+            , uniformsBlock uniforms
+            , varyingsBlock varyings
+            , allConstants
+            , allFunctions
+            , stripExtraWhitespace source
+            ]
 
 
 attributesBlock : List Attribute -> String
@@ -123,19 +185,9 @@ attributesBlock attributes =
     String.join "\n" (List.map attributeLine attributes)
 
 
-attributeLine : Attribute -> String
-attributeLine (Attribute name (Type typeName)) =
-    "attribute " ++ typeName ++ " " ++ name ++ ";"
-
-
 uniformsBlock : List Uniform -> String
 uniformsBlock uniforms =
     String.join "\n" (List.map uniformLine uniforms)
-
-
-uniformLine : Uniform -> String
-uniformLine (Uniform name (Type typeName)) =
-    "uniform " ++ typeName ++ " " ++ name ++ ";"
 
 
 varyingsBlock : List Varying -> String
@@ -143,8 +195,18 @@ varyingsBlock varyings =
     String.join "\n" (List.map varyingLine varyings)
 
 
+attributeLine : Attribute -> String
+attributeLine (Attribute (Type typeName) name) =
+    "attribute " ++ typeName ++ " " ++ name ++ ";"
+
+
+uniformLine : Uniform -> String
+uniformLine (Uniform (Type typeName) name) =
+    "uniform " ++ typeName ++ " " ++ name ++ ";"
+
+
 varyingLine : Varying -> String
-varyingLine (Varying name (Type typeName)) =
+varyingLine (Varying (Type typeName) name) =
     "varying " ++ typeName ++ " " ++ name ++ ";"
 
 
