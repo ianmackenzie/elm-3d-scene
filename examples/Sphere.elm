a4578605a918d28fc1619b44f8689cee572e99c3
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-01-03 22:25:50 +0100

    Update Sphere example

--- examples/Sphere.elm
+++ examples/Sphere.elm
@@ -9,6 +9,7 @@ import Illuminance
 import Length exposing (Meters)
 import Luminance
 import LuminousFlux
+import Palette.Tango as Tango
 import Parameter1d
 import Pixels
 import Point3d
@@ -17,8 +18,8 @@ import Scene3d
 import Scene3d.Chromaticity as Chromaticity
 import Scene3d.Drawable as Drawable
 import Scene3d.Exposure as Exposure
-import Scene3d.Light as Light
-import Scene3d.Mesh as Mesh exposing (Mesh, NoTangents, NoUV, ShadowsDisabled, Triangles, WithNormals)
+import Scene3d.LightSource as LightSource
+import Scene3d.Mesh as Mesh exposing (Mesh, Yes)
 import Scene3d.Shape as Shape
 import Temperature
 import Viewpoint3d exposing (Viewpoint3d)
@@ -28,7 +29,7 @@ type World
     = World
 
 
-mesh : Mesh World (Triangles WithNormals NoUV NoTangents ShadowsDisabled)
+mesh : Mesh World { hasNormals : Yes }
 mesh =
     Shape.sphere
         { radius = Length.centimeters 5
@@ -46,18 +47,10 @@ viewpoint =
 
 
 sunlight =
-    Light.directional
-        Chromaticity.daylight
+    LightSource.directionalLight
+        Chromaticity.d65
         (Illuminance.lux 10000)
         (Direction3d.yz (Angle.degrees -120))
-
-
-ambientLighting =
-    Light.overcast
-        { chromaticity = Chromaticity.daylight
-        , zenithDirection = Direction3d.positiveZ
-        , zenithLuminance = Luminance.nits 5000
-        }
 
 
 camera : Camera3d Meters World
@@ -70,7 +63,7 @@ camera =
 
 
 material =
-    { baseColor = Color.blue
+    { baseColor = Tango.skyBlue2
     , roughness = 0.4
     , metallic = False
     }
@@ -78,16 +71,21 @@ material =
 
 main : Html msg
 main =
-    Scene3d.render []
+    Scene3d.toHtml
         { camera = camera
         , width = Pixels.pixels 800
         , height = Pixels.pixels 600
-        , ambientLighting = Just ambientLighting
-        , lights =
-            Scene3d.oneLight sunlight { castsShadows = False }
+        , environmentalLighting =
+            Scene3d.softLighting
+                { upDirection = Direction3d.positiveZ
+                , above = ( Luminance.nits 5000, Chromaticity.d65 )
+                , below = ( Quantity.zero, Chromaticity.d65 )
+                }
+        , directLighting =
+            Scene3d.oneLightSource sunlight { castsShadows = False }
         , exposure =
-            Exposure.fromMaxLuminance
-                (Luminance.nits 5000)
-        , whiteBalance = Chromaticity.daylight
+            Exposure.fromMaxLuminance (Luminance.nits 5000)
+        , whiteBalance = Scene3d.defaultWhiteBalance
+        , backgroundColor = Scene3d.transparentBackground
         }
         [ Drawable.physical material mesh ]
