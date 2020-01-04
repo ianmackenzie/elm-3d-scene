64dbeff4bc40fb91931c6f025bef15048b31ed60
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-01-05 00:12:18 +0100

    Finish Material module implementation

--- src/Scene3d/Material.elm
+++ src/Scene3d/Material.elm
@@ -29,52 +29,60 @@ module Scene3d.Material exposing
 
 import Color exposing (Color)
 import Luminance exposing (Luminance)
+import Math.Vector3 exposing (Vec3)
 import Scene3d.Chromaticity exposing (Chromaticity)
+import Scene3d.ColorConversions as ColorConversions
 import Scene3d.Mesh exposing (Yes)
+import Scene3d.Types as Types exposing (LinearRgb(..))
 
 
-type Material properties
-    = SolidColor Color
-    | Emissive Luminance Chromaticity
-    | Diffuse Color
-    | Pbr { baseColor : Color, roughness : Float, metallic : Float }
-
-
-
--- | SolidColorTexture Texture
--- | EmissiveTexture Luminance Texture
--- | DiffuseTexture Texture
--- | PbrTextures { baseColor : Texture, roughness : Texture, metallic : Float }
+type alias Material properties =
+    Types.Material properties
 
 
 solidColor : Color -> Material properties
-solidColor =
-    SolidColor
+solidColor givenColor =
+    let
+        ( red, green, blue ) =
+            Color.toRGB givenColor
+    in
+    Types.ConstantMaterial <|
+        Math.Vector3.vec3 (red / 255) (green / 255) (blue / 255)
 
 
 emissive : Luminance -> Chromaticity -> Material properties
-emissive =
-    Emissive
+emissive luminance chromaticity =
+    let
+        (LinearRgb chromaticityRgb) =
+            ColorConversions.chromaticityToLinearRgb chromaticity
+
+        nits =
+            Luminance.inNits luminance
+    in
+    Types.EmissiveMaterial (LinearRgb (Math.Vector3.scale nits chromaticityRgb))
 
 
 diffuse : Color -> Material { a | hasNormals : Yes }
-diffuse =
-    Diffuse
+diffuse givenColor =
+    Types.LambertianMaterial (ColorConversions.colorToLinearRgb givenColor)
 
 
 metal : { baseColor : Color, roughness : Float } -> Material { a | hasNormals : Yes }
 metal { baseColor, roughness } =
-    Pbr { baseColor = baseColor, roughness = roughness, metallic = 1 }
+    hybrid { baseColor = baseColor, roughness = roughness, metallic = 1 }
 
 
 nonmetal : { baseColor : Color, roughness : Float } -> Material { a | hasNormals : Yes }
 nonmetal { baseColor, roughness } =
-    Pbr { baseColor = baseColor, roughness = roughness, metallic = 0 }
+    hybrid { baseColor = baseColor, roughness = roughness, metallic = 0 }
 
 
 hybrid : { baseColor : Color, roughness : Float, metallic : Float } -> Material { a | hasNormals : Yes }
-hybrid arguments =
-    Pbr arguments
+hybrid { baseColor, roughness, metallic } =
+    Types.PbrMaterial
+        (ColorConversions.colorToLinearRgb baseColor)
+        (clamp 0 1 roughness)
+        (clamp 0 1 metallic)
 
 
 
