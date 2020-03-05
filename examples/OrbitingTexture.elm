f50d522ecb8a419f819ff5efdac49e458ddbbd9f
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-03-05 16:30:30 -0500

    Switch OrbitingTexture example to use simple linear texture interpolation
    Mipmapping looked bad at glancing angles, at bit of aliasing is better (linearMipmapLinear is also pretty good - avoids aliasing, but at the cost of introducing blurriness instead)

--- examples/OrbitingTexture.elm
+++ examples/OrbitingTexture.elm
@@ -48,13 +48,20 @@ type Msg
 
 init : () -> ( Model, Cmd Msg )
 init () =
+    let
+        defaultTextureOptions =
+            Texture.defaultOptions
+
+        textureOptions =
+            { defaultTextureOptions | minify = Texture.linear }
+    in
     ( { azimuth = Angle.degrees 45
       , elevation = Angle.degrees 30
       , orbiting = False
       , texture = Nothing
       }
     , Task.attempt GotTexture
-        (Material.load "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Elm_logo.svg/512px-Elm_logo.svg.png")
+        (Material.loadWith textureOptions "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Elm_logo.svg/512px-Elm_logo.svg.png")
     )
 
 
