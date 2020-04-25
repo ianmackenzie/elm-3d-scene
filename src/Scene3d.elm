a3703a595cf9c16570c8335a5af0bbd23ca3f2c7
Ian Mackenzie <ian.mackenzie@arup.com>
2020-04-25 16:08:40 -0400

    Fix typo

--- src/Scene3d.elm
+++ src/Scene3d.elm
@@ -1176,7 +1176,7 @@ fullScreenQuadVertexShader =
         attribute vec2 position;
 
         void main() {
-            gl_Position = vec3(position, 0.0);
+            gl_Position = vec4(position, 0.0, 1.0);
         }
     |]
 
