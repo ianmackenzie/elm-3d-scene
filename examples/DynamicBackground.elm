910a0d4aa18b33234191988f731d3e0729b7f3c8
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-05-31 12:08:22 -0400

    Refactor example for readability

--- examples/DynamicBackground.elm
+++ examples/DynamicBackground.elm
@@ -24,94 +24,123 @@ import Temperature
 import Viewpoint3d exposing (Viewpoint3d)
 
 
-type WorldCoordinates
-    = WorldCoordinates
-
-
-viewpoint : Viewpoint3d Meters WorldCoordinates
-viewpoint =
-    Viewpoint3d.lookAt
-        { focalPoint = Point3d.origin
-        , eyePoint = Point3d.centimeters 20 10 20
-        , upDirection = Direction3d.positiveZ
-        }
-
-
-sunlight : Light WorldCoordinates Bool
-sunlight =
-    Light.directional (Light.castsShadows False)
-        { chromaticity = Light.daylight
-        , intensity = Illuminance.lux 10000
-        , direction = Direction3d.yz (Angle.degrees -120)
-        }
-
-
-overheadLighting : Color -> Light WorldCoordinates Never
-overheadLighting color =
-    Light.overhead
-        { upDirection = Direction3d.positiveZ
-        , chromaticity = Light.color color
-        , intensity = Illuminance.lux 10000
-        }
-
-
-camera : Camera3d Meters WorldCoordinates
-camera =
-    Camera3d.perspective
-        { viewpoint = viewpoint
-        , verticalFieldOfView = Angle.degrees 30
-        }
-
-
-material : Material.Textured coordinates
-material =
-    Material.nonmetal
-        { baseColor = Color.blue
-        , roughness = 0.4
-        }
-
-
+{-| Compute background color based on total elapsed time
+-}
 backgroundColor : Duration -> Color
 backgroundColor elapsedTime =
     let
         original =
             Color.toHsla Color.blue
 
+        -- Perform a full 'hue rotation' every 24 seconds
         incrementedHue =
             original.hue + Duration.inSeconds elapsedTime / 24
 
+        -- Adjust hue to be in range 0-1
         adjustedHue =
             incrementedHue - toFloat (floor incrementedHue)
     in
     Color.fromHsla { original | hue = adjustedHue }
 
 
-main : Program () Duration Duration
+{-| Store total elapsed time in our model
+-}
+type alias Model =
+    { elapsedTime : Duration
+    }
+
+
+{-| Receive a Tick message on every animation frame with elapsed duration since
+last frame (should usually be around 16 milliseconds)
+-}
+type Msg
+    = Tick Duration
+
+
+{-| Initialize to zero elapsed time
+-}
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( { elapsedTime = Quantity.zero }, Cmd.none )
+
+
+{-| When we receive a Tick message, increment the total elapsed time
+-}
+update : Msg -> Model -> ( Model, Cmd Msg )
+update (Tick duration) model =
+    ( { model | elapsedTime = model.elapsedTime |> Quantity.plus duration }
+    , Cmd.none
+    )
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    -- Convert Float value from onAnimationFrameDelta (a number of milliseconds)
+    -- into a typed Duration value, then wrap in a Tick message
+    Browser.Events.onAnimationFrameDelta (Duration.milliseconds >> Tick)
+
+
+view : Model -> Html Msg
+view model =
+    let
+        -- Get background color based on current time
+        currentBackgroundColor =
+            backgroundColor model.elapsedTime
+
+        -- Create a fixed directional light
+        sunlight =
+            Light.directional (Light.castsShadows False)
+                { chromaticity = Light.daylight
+                , intensity = Illuminance.lux 10000
+                , direction = Direction3d.yz (Angle.degrees -120)
+                }
+
+        -- Create some soft overhead lighting the same color as the background,
+        -- so that the sphere appears to be lit by its surroundings
+        overheadLighting =
+            Light.overhead
+                { upDirection = Direction3d.positiveZ
+                , chromaticity = Light.color currentBackgroundColor
+                , intensity = Illuminance.lux 10000
+                }
+
+        sphereMaterial =
+            Material.nonmetal
+                { baseColor = Color.blue
+                , roughness = 0.4
+                }
+    in
+    Scene3d.custom
+        { camera =
+            Camera3d.perspective
+                { viewpoint =
+                    Viewpoint3d.lookAt
+                        { focalPoint = Point3d.origin
+                        , eyePoint = Point3d.centimeters 20 10 20
+                        , upDirection = Direction3d.positiveZ
+                        }
+                , verticalFieldOfView = Angle.degrees 30
+                }
+        , clipDepth = Length.centimeters 0.5
+        , dimensions = ( Pixels.pixels 400, Pixels.pixels 300 )
+        , antialiasing = Scene3d.multisampling
+        , lights = Scene3d.twoLights sunlight overheadLighting
+        , exposure = Scene3d.exposureValue 12
+        , toneMapping = Scene3d.noToneMapping
+        , whiteBalance = Light.daylight
+        , background = Scene3d.backgroundColor currentBackgroundColor
+        , entities =
+            [ Scene3d.sphere sphereMaterial <|
+                Sphere3d.withRadius (Length.centimeters 5) Point3d.origin
+            ]
+        }
+
+
+main : Program () Model Msg
 main =
     Browser.element
-        { init = always ( Quantity.zero, Cmd.none )
-        , update =
-            \delta elapsed -> ( elapsed |> Quantity.plus delta, Cmd.none )
-        , subscriptions = always (Browser.Events.onAnimationFrameDelta Duration.milliseconds)
-        , view =
-            \elapsedTime ->
-                let
-                    currentBackgroundColor =
-                        backgroundColor elapsedTime
-                in
-                Scene3d.custom
-                    { camera = camera
-                    , clipDepth = Length.centimeters 0.5
-                    , dimensions = ( Pixels.pixels 800, Pixels.pixels 600 )
-                    , antialiasing = Scene3d.multisampling
-                    , lights = Scene3d.twoLights sunlight (overheadLighting currentBackgroundColor)
-                    , exposure = Scene3d.maxLuminance (Luminance.nits 5000)
-                    , toneMapping = Scene3d.noToneMapping
-                    , whiteBalance = Light.daylight
-                    , background = Scene3d.backgroundColor currentBackgroundColor
-                    , entities =
-                        [ Scene3d.sphere material <|
-                            Sphere3d.withRadius (Length.centimeters 5) Point3d.origin
-                        ]
-                    }
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
         }
