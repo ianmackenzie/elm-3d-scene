e9b71bd489538aaea33e3ef7e016a18cbc3caded
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-05-31 13:19:02 -0400

    Add comments to Points example

--- examples/Points.elm
+++ examples/Points.elm
@@ -18,12 +18,20 @@ import Viewpoint3d exposing (Viewpoint3d)
 main : Html msg
 main =
     let
-        mesh =
-            Mesh.points { radius = Pixels.pixels 5 } <|
-                Parameter1d.steps 12 <|
-                    Point3d.interpolateFrom
-                        (Point3d.meters 1 0 -1)
-                        (Point3d.meters -1 0 1)
+        -- Generate 13 evenly-spaced points by taking 12 steps from (1, 0, -1)
+        -- to (-1, 0, 1); see https://package.elm-lang.org/packages/ianmackenzie/elm-1d-parameter/latest/Parameter1d#steps
+        -- and https://package.elm-lang.org/packages/ianmackenzie/elm-geometry/latest/Point3d#interpolateFrom
+        -- for details
+        points =
+            Parameter1d.steps 12 <|
+                Point3d.interpolateFrom
+                    (Point3d.meters 1 0 -1)
+                    (Point3d.meters -1 0 1)
+
+        -- Create a Mesh value from the list of points, passing in the radius
+        -- in pixels which they should be rendered at
+        pointsMesh =
+            Mesh.points { radius = Pixels.pixels 5 } points
 
         camera =
             Camera3d.perspective
@@ -41,5 +49,7 @@ main =
         , dimensions = ( Pixels.pixels 300, Pixels.pixels 300 )
         , background = Scene3d.transparentBackground
         , clipDepth = Length.meters 0.1
-        , entities = [ Scene3d.mesh (Material.color Color.blue) mesh ]
+        , entities =
+            -- Color the points blue
+            [ Scene3d.mesh (Material.color Color.blue) pointsMesh ]
         }
