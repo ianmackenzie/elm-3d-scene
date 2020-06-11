a3515f98592674ded583d72b3d89a50998a8a9d8
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-06-10 21:43:42 -0400

    Clean up multiple shadows example

--- examples/MultipleShadows.elm
+++ examples/MultipleShadows.elm
@@ -5,11 +5,10 @@ import Array exposing (Array)
 import Axis3d
 import Block3d exposing (Block3d)
 import Browser
-import Browser.Dom exposing (getViewport)
-import Browser.Events exposing (onAnimationFrameDelta, onMouseDown, onMouseMove, onMouseUp, onResize, onVisibilityChange)
+import Browser.Dom
+import Browser.Events
 import Camera3d
 import Color
-import Common.Materials as Materials
 import Dict exposing (Dict)
 import Direction3d
 import Duration exposing (Duration)
@@ -36,25 +35,25 @@ import Vector3d
 import Viewpoint3d
 
 
+type WorldCoordinates
+    = WorldCoordinates
+
+
 type alias Model =
     { width : Quantity Float Pixels
     , height : Quantity Float Pixels
     , elapsedTime : Duration
-    , activeDrag : Maybe ( Float, Float )
+    , orbiting : Bool
     , azimuth : Angle
     , elevation : Angle
     }
 
 
-type WorldCoordinates
-    = WorldCoordinates
-
-
 type Msg
     = Resize (Quantity Float Pixels) (Quantity Float Pixels)
     | Tick Duration
-    | MouseDown ( Float, Float )
-    | MouseMove ( Float, Float )
+    | MouseDown
+    | MouseMove (Quantity Float Pixels) (Quantity Float Pixels)
     | MouseUp
     | VisibilityChange Browser.Events.Visibility
 
@@ -62,27 +61,30 @@ type Msg
 main : Program () Model Msg
 main =
     Browser.element
-        { init =
-            \_ ->
-                ( { width = Quantity.zero
-                  , height = Quantity.zero
-                  , elapsedTime = Quantity.zero
-                  , activeDrag = Nothing
-                  , azimuth = Angle.degrees -90
-                  , elevation = Angle.degrees 30
-                  }
-                , Task.perform
-                    (\{ viewport } ->
-                        Resize
-                            (Pixels.pixels viewport.width)
-                            (Pixels.pixels viewport.height)
-                    )
-                    getViewport
-                )
-        , subscriptions = subscriptions
+        { init = init
         , update = update
+        , subscriptions = subscriptions
         , view = view
         }
+
+
+init : () -> ( Model, Cmd Msg )
+init () =
+    ( { width = Quantity.zero
+      , height = Quantity.zero
+      , elapsedTime = Quantity.zero
+      , orbiting = False
+      , azimuth = Angle.degrees -90
+      , elevation = Angle.degrees 30
+      }
+    , Task.perform
+        (\{ viewport } ->
+            Resize
+                (Pixels.pixels viewport.width)
+                (Pixels.pixels viewport.height)
+        )
+        Browser.Dom.getViewport
+    )
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
@@ -94,117 +96,136 @@ update message model =
         Tick elapsed ->
             ( { model | elapsedTime = model.elapsedTime |> Quantity.plus elapsed }, Cmd.none )
 
-        MouseDown position ->
-            ( { model | activeDrag = Just position }, Cmd.none )
+        MouseDown ->
+            ( { model | orbiting = True }, Cmd.none )
 
         MouseUp ->
-            ( { model | activeDrag = Nothing }, Cmd.none )
+            ( { model | orbiting = False }, Cmd.none )
 
         VisibilityChange Browser.Events.Visible ->
             ( model, Cmd.none )
 
         VisibilityChange Browser.Events.Hidden ->
-            ( { model | activeDrag = Nothing }, Cmd.none )
+            ( { model | orbiting = False }, Cmd.none )
 
-        MouseMove ( x, y ) ->
-            case model.activeDrag of
-                Just ( previousX, previousY ) ->
-                    let
-                        deltaX =
-                            x - previousX
+        MouseMove dx dy ->
+            if model.orbiting then
+                let
+                    rotationRate =
+                        Angle.degrees 0.5 |> Quantity.per Pixels.pixel
 
-                        deltaY =
-                            y - previousY
+                    newAzimuth =
+                        model.azimuth
+                            |> Quantity.minus (dx |> Quantity.at rotationRate)
 
-                        newAzimuth =
-                            model.azimuth
-                                |> Quantity.minus
-                                    (Angle.degrees 0.5 |> Quantity.multiplyBy deltaX)
+                    newElevation =
+                        model.elevation
+                            |> Quantity.plus (dy |> Quantity.at rotationRate)
+                            |> Quantity.clamp (Angle.degrees 5) (Angle.degrees 85)
+                in
+                ( { model
+                    | orbiting = True
+                    , azimuth = newAzimuth
+                    , elevation = newElevation
+                  }
+                , Cmd.none
+                )
 
-                        newElevation =
-                            model.elevation
-                                |> Quantity.plus
-                                    (Angle.degrees 0.5 |> Quantity.multiplyBy deltaY)
-                                |> Quantity.clamp (Angle.degrees 0) (Angle.degrees 90)
-                    in
-                    ( { model
-                        | activeDrag = Just ( x, y )
-                        , azimuth = newAzimuth
-                        , elevation = newElevation
-                      }
-                    , Cmd.none
-                    )
-
-                Nothing ->
-                    ( model, Cmd.none )
+            else
+                ( model, Cmd.none )
 
 
-mouseEventDecoder : Decoder ( Float, Float )
-mouseEventDecoder =
-    Decode.map2 Tuple.pair
-        (Decode.field "clientX" Decode.float)
-        (Decode.field "clientY" Decode.float)
+mouseMoveDecoder : Decoder Msg
+mouseMoveDecoder =
+    Decode.map2 MouseMove
+        (Decode.field "movementX" (Decode.map Pixels.pixels Decode.float))
+        (Decode.field "movementY" (Decode.map Pixels.pixels Decode.float))
 
 
 subscriptions : Model -> Sub Msg
 subscriptions model =
     Sub.batch
-        [ onResize (\w h -> Resize (Pixels.pixels (toFloat w)) (Pixels.pixels (toFloat h)))
-        , onAnimationFrameDelta (Duration.milliseconds >> Tick)
-        , onVisibilityChange VisibilityChange
-        , if model.activeDrag == Nothing then
-            Sub.none
+        [ -- Listen for resize events so we can render full screen
+          Browser.Events.onResize
+            (\width height ->
+                Resize
+                    (Pixels.pixels (toFloat width))
+                    (Pixels.pixels (toFloat height))
+            )
+
+        -- Subscribe to animation frames to animate the cubes
+        , Browser.Events.onAnimationFrameDelta (Duration.milliseconds >> Tick)
+
+        -- Listen for visibility change events so we can stop orbiting if the
+        -- user switches to a different tab etc.
+        , Browser.Events.onVisibilityChange VisibilityChange
+
+        -- Listen for orbit-related mouse events
+        , if model.orbiting then
+            Sub.batch
+                [ Browser.Events.onMouseMove mouseMoveDecoder
+                , Browser.Events.onMouseUp (Decode.succeed MouseUp)
+                ]
 
           else
-            onMouseMove (Decode.map MouseMove mouseEventDecoder)
-        , onMouseDown (Decode.map MouseDown mouseEventDecoder)
-        , onMouseUp (Decode.succeed MouseUp)
+            Browser.Events.onMouseDown (Decode.succeed MouseDown)
         ]
 
 
-
--- VIEW
-
-
+{-| Create both a Light and an Entity (a bright glowing sphere) representing a
+particular point light
+-}
 pointLight :
-    { position : Point3d Meters WorldCoordinates, chromaticity : Chromaticity, intensity : LuminousFlux }
+    { position : Point3d Meters WorldCoordinates
+    , chromaticity : Chromaticity
+    , intensity : LuminousFlux
+    }
     -> ( Light WorldCoordinates Bool, Entity WorldCoordinates )
 pointLight properties =
     let
-        sphereRadius =
-            Length.centimeters 5
+        -- Create a sphere to represent a light bulb
+        sphere =
+            Sphere3d.atPoint properties.position (Length.millimeters 5)
 
-        surfaceArea =
-            Sphere3d.atOrigin sphereRadius |> Sphere3d.surfaceArea
-
+        -- Calculate the luminance of the sphere surface by dividing the given
+        -- total luminous flux of the light by the surface area of the sphere
+        -- and by the solid angle of a hemisphere (assuming that each point on
+        -- the surface of the bulb emits light equally in all directions)...I
+        -- am not 100% sure this is exactly correct =)
         sphereLuminance =
             properties.intensity
-                |> Quantity.per (SolidAngle.spats 1)
-                |> Quantity.per surfaceArea
+                |> Quantity.per (SolidAngle.spats 0.5)
+                |> Quantity.per (Sphere3d.surfaceArea sphere)
+
+        -- Create an emissive (glowing) material for the sphere
+        sphereMaterial =
+            Material.emissive properties.chromaticity sphereLuminance
     in
     ( Light.point (Light.castsShadows True) properties
-    , Scene3d.sphere (Material.emissive properties.chromaticity sphereLuminance)
-        (Sphere3d.atPoint properties.position sphereRadius)
+    , Scene3d.sphere sphereMaterial sphere
     )
 
 
 view : Model -> Html Msg
-view { width, height, elapsedTime, azimuth, elevation } =
+view model =
     let
+        -- Incandescent light bulb
         ( firstLight, firstLightBall ) =
             pointLight
-                { position = Point3d.meters -2 -2 3
+                { position = Point3d.centimeters -50 -30 70
                 , chromaticity = Light.incandescent
-                , intensity = LuminousFlux.lumens 50000
+                , intensity = LuminousFlux.lumens 500
                 }
 
+        -- Fluorescent light bulb
         ( secondLight, secondLightBall ) =
             pointLight
-                { position = Point3d.meters -1 2 5
+                { position = Point3d.centimeters -40 10 90
                 , chromaticity = Light.fluorescent
-                , intensity = LuminousFlux.lumens 50000
+                , intensity = LuminousFlux.lumens 500
                 }
 
+        -- Rough approximation of unlight near sunset
         thirdLight =
             Light.directional (Light.castsShadows True)
                 { direction = Direction3d.xyZ (Angle.degrees -90) (Angle.degrees -45)
@@ -212,65 +233,7 @@ view { width, height, elapsedTime, azimuth, elevation } =
                 , intensity = Illuminance.lux 30
                 }
 
-        viewpoint =
-            Viewpoint3d.orbit
-                { focalPoint = Point3d.meters 4 2 -2
-                , groundPlane = SketchPlane3d.xy
-                , azimuth = azimuth
-                , elevation = elevation
-                , distance = Length.meters 20
-                }
-
-        camera =
-            Camera3d.perspective
-                { viewpoint = viewpoint
-                , verticalFieldOfView = Angle.degrees 45
-                }
-
-        plane =
-            Scene3d.quad (Material.matte Color.gray)
-                (Point3d.meters -9 -9 0)
-                (Point3d.meters 9 -9 0)
-                (Point3d.meters 9 9 0)
-                (Point3d.meters -9 9 0)
-                |> Scene3d.translateBy (Vector3d.meters 3 1 -4)
-
-        firstBoxRotationAxis =
-            Axis3d.through Point3d.origin <|
-                Direction3d.xyZ (Angle.degrees 45)
-                    (Angle.atan2 (Quantity.float 1) (Quantity.float (sqrt 2)))
-
-        firstBoxRotationSpeed =
-            Angle.degrees 30 |> Quantity.per Duration.second
-
-        boxDimensions =
-            ( Length.meters 2
-            , Length.meters 2
-            , Length.meters 2
-            )
-
-        firstBox =
-            Scene3d.blockWithShadow Materials.aluminum
-                (Block3d.centeredOn Frame3d.atOrigin boxDimensions)
-                |> Scene3d.rotateAround firstBoxRotationAxis
-                    (elapsedTime |> Quantity.at firstBoxRotationSpeed)
-
-        secondBoxCenter =
-            Point3d.meters 6 1 -3
-
-        secondBoxRotationAxis =
-            Axis3d.through secondBoxCenter Direction3d.positiveZ
-
-        secondBoxRotationSpeed =
-            Angle.degrees 30 |> Quantity.per Duration.second
-
-        secondBox =
-            Scene3d.blockWithShadow
-                (Material.pbr { baseColor = Color.blue, roughness = 0.25, metallic = 0 })
-                (Block3d.centeredOn (Frame3d.atPoint secondBoxCenter) boxDimensions)
-                |> Scene3d.rotateAround secondBoxRotationAxis
-                    (elapsedTime |> Quantity.at secondBoxRotationSpeed)
-
+        -- Add some soft lighting to fill in shadowed areas
         softLighting =
             Light.soft
                 { upDirection = Direction3d.positiveZ
@@ -278,16 +241,91 @@ view { width, height, elapsedTime, azimuth, elevation } =
                 , intensityAbove = Illuminance.lux 30
                 , intensityBelow = Illuminance.lux 5
                 }
+
+        -- Create a quad to act as a 'floor'
+        plane =
+            Scene3d.quad (Material.matte Color.gray)
+                (Point3d.centimeters -90 -90 0)
+                (Point3d.centimeters 90 -90 0)
+                (Point3d.centimeters 90 90 0)
+                (Point3d.centimeters -90 90 0)
+
+        -- Define overall box dimensions for the two boxes
+        boxDimensions =
+            ( Length.centimeters 20
+            , Length.centimeters 20
+            , Length.centimeters 20
+            )
+
+        -- Create the floating aluminum-colored box
+        firstBoxCenter =
+            Point3d.centimeters -30 -10 40
+
+        firstBoxRotationAxis =
+            Axis3d.through firstBoxCenter <|
+                Direction3d.xyZ (Angle.degrees 45)
+                    (Angle.atan2 (Quantity.float 1) (Quantity.float (sqrt 2)))
+
+        firstBoxRotationSpeed =
+            Angle.degrees 30 |> Quantity.per Duration.second
+
+        firstBoxAngle =
+            model.elapsedTime |> Quantity.at firstBoxRotationSpeed
+
+        firstBoxMaterial =
+            Material.metal
+                { baseColor = Color.rgb255 233 235 236
+                , roughness = 0.6
+                }
+
+        firstBox =
+            Scene3d.blockWithShadow firstBoxMaterial
+                (Block3d.centeredOn (Frame3d.atPoint firstBoxCenter) boxDimensions)
+                |> Scene3d.rotateAround firstBoxRotationAxis firstBoxAngle
+
+        -- Create the blue box sitting on the 'floor'
+        secondBoxCenter =
+            Point3d.centimeters 30 0 10
+
+        secondBoxRotationAxis =
+            Axis3d.through secondBoxCenter Direction3d.positiveZ
+
+        secondBoxRotationSpeed =
+            Angle.degrees 30 |> Quantity.per Duration.second
+
+        secondBoxAngle =
+            model.elapsedTime |> Quantity.at secondBoxRotationSpeed
+
+        bluePlastic =
+            Material.nonmetal { baseColor = Color.blue, roughness = 0.25 }
+
+        secondBox =
+            Scene3d.blockWithShadow bluePlastic
+                (Block3d.centeredOn (Frame3d.atPoint secondBoxCenter) boxDimensions)
+                |> Scene3d.rotateAround secondBoxRotationAxis secondBoxAngle
+
+        -- Define camera as usual
+        camera =
+            Camera3d.perspective
+                { viewpoint =
+                    Viewpoint3d.orbitZ
+                        { focalPoint = Point3d.centimeters 0 0 20
+                        , azimuth = model.azimuth
+                        , elevation = model.elevation
+                        , distance = Length.meters 3
+                        }
+                , verticalFieldOfView = Angle.degrees 30
+                }
     in
     Scene3d.custom
         { lights = Scene3d.fourLights firstLight secondLight thirdLight softLighting
         , camera = camera
-        , clipDepth = Length.meters 1
+        , clipDepth = Length.centimeters 10
         , exposure = Scene3d.exposureValue 6
         , toneMapping = Scene3d.hableFilmicToneMapping
         , whiteBalance = Light.fluorescent
         , antialiasing = Scene3d.multisampling
-        , dimensions = ( width, height )
+        , dimensions = ( model.width, model.height )
         , background = Scene3d.backgroundColor Color.lightBlue
         , entities = [ plane, firstBox, secondBox, firstLightBall, secondLightBall ]
         }
