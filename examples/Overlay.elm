809e09ef15297a641303041f899c2db63583a316
Ian Mackenzie <ian.e.mackenzie@gmail.com>
2020-10-11 15:51:29 -0400

    Inline shape definition into Overlay example
    Necessary to allow it to be published as an Ellie

--- examples/Overlay.elm
+++ examples/Overlay.elm
@@ -1,33 +1,38 @@
 module Overlay exposing (..)
 
 import Angle exposing (Angle)
+import Array exposing (Array)
 import Axis2d exposing (Axis2d)
 import Axis3d
 import Browser
 import Camera3d
 import Circle2d
+import Color exposing (Color)
 import Direction2d exposing (Direction2d)
 import Direction3d
 import Element
 import Element.Border
 import Element.Input as Input
 import Frame2d
+import Frame3d exposing (Frame3d)
 import Geometry.Svg as Svg
 import Html exposing (Html)
 import Html.Attributes as Attributes
-import Length
-import LineSegment3d
+import Length exposing (Length, Meters)
+import LineSegment3d exposing (LineSegment3d)
 import LineSegment3d.Projection as LineSegment3d
-import Logo
 import Pixels exposing (Pixels)
 import Point2d
-import Point3d
+import Point3d exposing (Point3d)
 import Point3d.Projection as Point3d
 import Quantity
 import Rectangle2d
 import Scene3d
+import Scene3d.Material as Material
+import Scene3d.Mesh as Mesh
 import Svg exposing (Svg)
 import Svg.Attributes
+import TriangularMesh exposing (TriangularMesh)
 import Vector2d
 import Viewpoint3d
 import WebGL
@@ -103,7 +108,7 @@ view { angle, projectionType } =
 
         -- Take the 3D model for the logo and rotate it by the current angle
         rotatedLogo =
-            Logo.entity |> Scene3d.rotateAround Axis3d.z angle
+            blockEntity |> Scene3d.rotateAround Axis3d.z angle
 
         -- Defines the shape of the 'screen' that we will be using when
         -- projecting 3D points into 2D
@@ -113,7 +118,7 @@ view { angle, projectionType } =
         -- Take all vertices of the logo shape, rotate them the same amount as
         -- the logo itself and then project them into 2D screen space
         vertices2d =
-            Logo.vertices
+            blockVertices
                 |> List.map (Point3d.rotateAround Axis3d.z angle)
                 |> List.map (Point3d.toScreenSpace camera screenRectangle)
 
@@ -132,7 +137,7 @@ view { angle, projectionType } =
 
         -- Project all logo edges and create SVG elements in a similar way
         svgLines =
-            Logo.edges
+            blockEdges
                 |> List.map (LineSegment3d.rotateAround Axis3d.z angle)
                 |> List.map (LineSegment3d.toScreenSpace camera screenRectangle)
                 |> List.map
@@ -243,3 +248,175 @@ main =
         , update = update
         , subscriptions = always Sub.none
         }
+
+
+
+---------- COLORED BLOCK SHAPE ----------
+
+
+type CenterCoordinates
+    = CenterCoordinates
+
+
+type CornerCoordinates
+    = CornerCoordinates
+
+
+blockHeight : Length
+blockHeight =
+    Length.meters 0.9
+
+
+p0 : Point3d Meters CornerCoordinates
+p0 =
+    Point3d.origin
+
+
+p1 : Point3d Meters CornerCoordinates
+p1 =
+    Point3d.meters 1 0 0
+
+
+p2 : Point3d Meters CornerCoordinates
+p2 =
+    Point3d.meters 1 1 0
+
+
+p3 : Point3d Meters CornerCoordinates
+p3 =
+    Point3d.meters 0 1 0
+
+
+p4 : Point3d Meters CornerCoordinates
+p4 =
+    Point3d.xyz Quantity.zero Length.meter blockHeight
+
+
+p5 : Point3d Meters CornerCoordinates
+p5 =
+    Point3d.xyz Quantity.zero Quantity.zero blockHeight
+
+
+p6 : Point3d Meters CornerCoordinates
+p6 =
+    Point3d.xyz Length.meter Quantity.zero blockHeight
+
+
+p7 : Point3d Meters CornerCoordinates
+p7 =
+    Point3d.xyz Length.meter (Length.meters 0.4) blockHeight
+
+
+p8 : Point3d Meters CornerCoordinates
+p8 =
+    Point3d.xyz Length.meter Length.meter (Length.meters 0.4)
+
+
+p9 : Point3d Meters CornerCoordinates
+p9 =
+    Point3d.xyz (Length.meters 0.4) Length.meter blockHeight
+
+
+blockCenterFrame : Frame3d Meters CornerCoordinates { defines : CenterCoordinates }
+blockCenterFrame =
+    Frame3d.atPoint <|
+        Point3d.xyz
+            (Length.meters 0.5)
+            (Length.meters 0.5)
+            (Quantity.half blockHeight)
+
+
+triangleFan : List (Point3d Meters coordinates) -> Mesh.Plain coordinates
+triangleFan points =
+    let
+        faceIndices =
+            List.range 1 (List.length points - 2)
+                |> List.map
+                    (\index ->
+                        ( 0, index, index + 1 )
+                    )
+    in
+    Mesh.indexedTriangles <|
+        TriangularMesh.indexed (Array.fromList points) faceIndices
+
+
+blockEntity : Scene3d.Entity CenterCoordinates
+blockEntity =
+    let
+        orange =
+            Color.rgb255 240 173 0
+
+        green =
+            Color.rgb255 127 209 59
+
+        lightBlue =
+            Color.rgb255 96 181 204
+
+        darkBlue =
+            Color.rgb255 90 99 86
+
+        leftFace =
+            Scene3d.mesh (Material.color orange) <|
+                triangleFan [ p1, p2, p8, p7, p6 ]
+
+        rightFace =
+            Scene3d.mesh (Material.color lightBlue) <|
+                triangleFan [ p2, p3, p4, p9, p8 ]
+
+        topFace =
+            Scene3d.mesh (Material.color green) <|
+                triangleFan [ p6, p7, p9, p4, p5 ]
+
+        triangleFace =
+            Scene3d.mesh (Material.color darkBlue) <|
+                triangleFan [ p7, p8, p9 ]
+
+        bottomFace =
+            Scene3d.mesh (Material.color green) <|
+                triangleFan [ p0, p3, p2, p1 ]
+
+        backLeftFace =
+            Scene3d.mesh (Material.color lightBlue) <|
+                triangleFan [ p6, p5, p0, p1 ]
+
+        backRightFace =
+            Scene3d.mesh (Material.color orange) <|
+                triangleFan [ p3, p0, p5, p4 ]
+    in
+    Scene3d.group
+        [ leftFace
+        , rightFace
+        , topFace
+        , triangleFace
+        , backLeftFace
+        , backRightFace
+        , bottomFace
+        ]
+        |> Scene3d.relativeTo blockCenterFrame
+
+
+blockVertices : List (Point3d Meters CenterCoordinates)
+blockVertices =
+    [ p0, p1, p2, p3, p4, p5, p6, p7, p8, p9 ]
+        |> List.map (Point3d.relativeTo blockCenterFrame)
+
+
+blockEdges : List (LineSegment3d Meters CenterCoordinates)
+blockEdges =
+    [ LineSegment3d.from p0 p1
+    , LineSegment3d.from p1 p2
+    , LineSegment3d.from p2 p3
+    , LineSegment3d.from p3 p0
+    , LineSegment3d.from p0 p5
+    , LineSegment3d.from p1 p6
+    , LineSegment3d.from p2 p8
+    , LineSegment3d.from p3 p4
+    , LineSegment3d.from p5 p6
+    , LineSegment3d.from p6 p7
+    , LineSegment3d.from p7 p8
+    , LineSegment3d.from p8 p9
+    , LineSegment3d.from p7 p9
+    , LineSegment3d.from p9 p4
+    , LineSegment3d.from p4 p5
+    ]
+        |> List.map (LineSegment3d.relativeTo blockCenterFrame)
