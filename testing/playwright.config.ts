85a7238a06eb2a99cdebb4b8e4ae76c0a0eac511
Andrey Kuzmin <hi@unsoundscapes.com>
2024-04-01 23:21:14 +0200

    Merge the reports

--- testing/playwright.config.ts
+++ testing/playwright.config.ts
@@ -13,12 +13,12 @@ export default defineConfig({
   fullyParallel: true,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  /* Do not retry the tests, the tests seem to be reliable */
+  retries: 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? "blob" : "html",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
