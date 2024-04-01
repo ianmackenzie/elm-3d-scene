1db868ae5fa4aac62e760542587162439c22e0fa
Andrey Kuzmin <hi@unsoundscapes.com>
2024-04-01 19:59:46 +0200

    Remove traces

--- testing/playwright.config.ts
+++ testing/playwright.config.ts
@@ -24,8 +24,8 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: `http://127.0.0.1:${PORT}`,
 
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: "on-first-retry",
+    /* Do not collect the trace. See https://playwright.dev/docs/trace-viewer */
+    trace: "off",
   },
 
   snapshotPathTemplate: "snapshots/{arg}{ext}",
